5a88a7afba6ecf6c95abe12ce9d177c9a285adb3
umayanga <umayangag@gmail.com>
2020-02-07 13:29:31 +0530

    main func done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,21 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
 public class Main {
+    public static void main(String args[]) {
+        System.out.println("start");
+
+        ArrayList<Float> numbers = new ArrayList<Float>();
+        String operator = "";
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter operator: ");
+
+        operator = myObj.nextLine();
+
+        for (String i : args) {
+            numbers.add(Float.parseFloat(i));
+        }
+    }
 }
